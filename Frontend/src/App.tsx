168f15b641c556b12375794841d3dd94e6fc2535
manasdeo <rmnslogin@gmail.com>
2024-07-31 21:27:54 +0530

    dotenv

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,5 @@
-import "./App.css";
-
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <div className="text-3xl font-bold underline">Hello World</div>;
 }
 
 export default App;
